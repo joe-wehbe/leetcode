e0db787d358db7ef6708f110464bd34585d34ecb
Joe Wehbe <joewehbe@yahoo.com>
2024-08-05 16:57:06 +0300

    Time: 7 ms (55.29%), Space: 44.2 MB (51.23%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
@@ -3,8 +3,7 @@ class Solution {
         
         LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
         for(String num : arr)
-            map.put(num, map.getOrDefault(num, 0) + 1);
-    
+            map.put(num, map.getOrDefault(num, 0) + 1);    
         
         int count = 0;
         for (Map.Entry<String, Integer> entry : map.entrySet()) {            
