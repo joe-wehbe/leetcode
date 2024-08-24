0b3d7c5a9ea53f5ff882985a27c4806ad0ee4b96
Joe Wehbe <joewehbe@yahoo.com>
2024-08-24 23:30:03 +0300

    Time: 0 ms (100.00%), Space: 41.3 MB (90.66%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        
+        for(int i = 97; i <= 122; i++){
+            if(!sentence.contains(Character.toString((char)i)))
+                return false;
+        }
+        return true;
+    }
+}
