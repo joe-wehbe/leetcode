2f822ca817e42bee49ea3fc76be2ff2764267282
Joe Wehbe <joewehbe@yahoo.com>
2024-10-01 11:12:40 +0300

    Time: 70 ms (20.41%), Space: 52.2 MB (61.22%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.ts
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.ts
@@ -3,9 +3,9 @@ function minOperations(nums: number[]): number {
     let operations = 0;
 
     for (let i = 1; i < nums.length; i++) {
-        while (nums[i] <= nums[i-1]) {
-            nums[i]++;
-            operations++;
+        if (nums[i] <= nums[i-1]) {
+            operations += nums[i - 1] - nums[i] + 1;
+            nums[i] = nums[i - 1] + 1;
         }
     }
 
