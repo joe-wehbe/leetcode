f28deaeffff0d10f8f04e0dd5c5c3d47d2d22739
Joe Wehbe <joewehbe@yahoo.com>
2024-10-22 16:20:55 +0300

    Time: 1 ms (98.67%), Space: 51.7 MB (53.33%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.ts
@@ -0,0 +1,18 @@
+function findMiddleIndex(nums: number[]): number {
+    
+    let totalSum = 0;
+    let leftSideSum = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        totalSum += nums[i];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSideSum === totalSum - leftSideSum - nums[i]) {
+            return i;
+        }
+        leftSideSum += nums[i];
+    }
+
+    return -1;
+}
