81fac86bf7b193d4326da4d0caf192d39a3645e2
Joe Wehbe <joewehbe@yahoo.com>
2024-11-06 13:23:53 +0200

    Time: 79 ms (100.00%), Space: 59.5 MB (25.00%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.ts
@@ -0,0 +1,37 @@
+function canSortArray(nums: number[]): boolean {  
+    
+    let setBitsArray: number[] = [];
+    for(const num of nums) {
+        setBitsArray.push(num.toString(2).split('1').length - 1);
+    }
+    
+    let segments: number[][] = [];
+    let currentSegment: number[] = []; 
+    for(let i = 0; i < nums.length; i++) {
+        if(currentSegment.length == 0) {
+            currentSegment.push(nums[i]);
+        }
+        else {
+            if(setBitsArray[i] == setBitsArray[i-1]) {
+                currentSegment.push(nums[i]);
+            }
+            else {
+                segments.push(currentSegment);
+                currentSegment = [nums[i]];
+            }
+        }
+    }
+    
+    if(currentSegment.length > 0) {
+        segments.push(currentSegment);
+    }
+    
+    console.log(segments);
+    
+    for(let i = 0; i < segments.length - 1; i++) {
+        if(Math.max(...segments[i]) > Math.min(...segments[i + 1])) return false;
+    }
+       
+    return true;
+}
+
