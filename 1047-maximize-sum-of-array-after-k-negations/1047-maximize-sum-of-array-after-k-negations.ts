f9a94330db3f6ec9316eb5400b58638f2afb2272
Joe Wehbe <joewehbe@yahoo.com>
2024-11-20 18:51:08 +0200

    Time: 110 ms (5.56%), Space: 56.9 MB (6.45%) - LeetHub

--- 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.ts
+++ 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.ts
@@ -1,7 +1,9 @@
 function largestSumAfterKNegations(nums: number[], k: number): number {
+    nums.sort((a, b) => a - b);
+
     while (k > 0) {
-        let minIndex = nums.indexOf(Math.min(...nums));
-        nums[minIndex] = -nums[minIndex];
+        nums[0] = -nums[0];
+        nums.sort((a, b) => a - b);
         k--;
     }
     return nums.reduce((acc, curr) => acc + curr, 0);
