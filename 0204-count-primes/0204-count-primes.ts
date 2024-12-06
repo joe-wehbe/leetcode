1f170caae8142b6fd7dcd94eb9f8d3349e385aad
Joe Wehbe <joewehbe@yahoo.com>
2024-12-06 16:43:15 +0200

    Time: 1811 ms (5.56%), Space: 52.1 MB (97.46%) - LeetHub

--- 0204-count-primes/0204-count-primes.ts
+++ 0204-count-primes/0204-count-primes.ts
@@ -14,7 +14,9 @@ function isPrime(n: number): boolean {
     if (n == 2) return true;
     if (n % 2 == 0) return false;
     
-    for(let i = 3; i <= Math.sqrt(n); i += 2) {
+    const sqrt = Math.sqrt(n);
+    
+    for(let i = 3; i <= sqrt; i += 2) {
         if (n % i === 0) return false 
     }
     return true;
