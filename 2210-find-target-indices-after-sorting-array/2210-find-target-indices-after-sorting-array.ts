b1920d7abca68e4f6d4d431fb3e4ce350393edb7
Joe Wehbe <joewehbe@yahoo.com>
2024-12-22 23:15:53 +0200

    Time: 1 ms (97.06%), Space: 51.9 MB (30.88%) - LeetHub

---
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.ts
@@ -0,0 +1,10 @@
+function targetIndices(nums: number[], target: number): number[] {
+    nums.sort((a, b) => a - b);
+    let result: number[] = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] == target) result.push(i);
+    }
+
+    return result;
+};
