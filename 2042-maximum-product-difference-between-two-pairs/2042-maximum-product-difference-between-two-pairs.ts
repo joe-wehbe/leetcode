a1577d010cc099b4c11eef1883a212fdf43436a0
Joe Wehbe <joewehbe@yahoo.com>
2024-12-28 21:56:57 +0200

    Time: 71 ms (67.57%), Space: 52.5 MB (75.68%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.ts
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.ts
@@ -1,4 +1,22 @@
 function maxProductDifference(nums: number[]): number {
-    nums.sort((a, b) => a - b);
-    return (nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1]);
-};
+    let max1 = -Infinity, max2 = -Infinity;
+    let min1 = Infinity, min2 = Infinity;
+
+    for (let num of nums) {
+        if (num > max1) {
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2) {
+            max2 = num;
+        }
+        
+        if (num < min1) {
+            min2 = min1;
+            min1 = num;
+        } else if (num < min2) {
+            min2 = num;
+        }
+    }
+
+    return (max1 * max2) - (min1 * min2);
+}
