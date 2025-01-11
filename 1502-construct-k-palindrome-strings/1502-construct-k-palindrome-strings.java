90669a370e66f3b7121eb7ca4ba1fff482d8b4db
Joe Wehbe <joewehbe@yahoo.com>
2025-01-11 23:55:07 +0200

    Time: 4 ms (87.99%), Space: 45.6 MB (19.16%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
         if (s.length() < k) return false;
-
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
         }
-
-        int count = 0;
-        for (Map.Entry<Character, Integer> entry: map.entrySet()) {
-            if (entry.getValue() % 2 != 0) {
-                count++;
+        
+        int oddCount = 0;
+        for (int count : freq) {
+            if (count % 2 != 0) {
+                oddCount++;
             }
         }
-
-        return count <= k;
+        
+        return oddCount <= k;
     }
 }
