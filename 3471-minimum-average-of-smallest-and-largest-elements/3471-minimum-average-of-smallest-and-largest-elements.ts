af672a2067079de5b83e46f952a915f633fe16ac
Joe Wehbe <joewehbe@yahoo.com>
2025-01-27 12:07:28 +0200

    Time: 3 ms (52.87%), Space: 52.5 MB (17.24%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.ts
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.ts
@@ -1,18 +1,13 @@
 function minimumAverage(nums: number[]): number {    
-    let n = nums.length / 2;
     let min = 0;
     let max = nums.length - 1;
-    let averages = [];
     let minAverage = 51;
 
     nums.sort((a, b) => a-b);
 
-    while (n > 0) {
+    while (min < max) {
         let average = (nums[min] + nums[max]) / 2;
-        averages.push(average);
         minAverage = Math.min(minAverage, average);
-        
-        n--;
         min++;
         max--;
     }
