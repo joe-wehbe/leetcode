df4e5e76dee0dc60f6c96e88708fad604a4aaa3a
Joe Wehbe <joewehbe@yahoo.com>
2025-02-11 17:36:46 +0200

    Time: 33 ms (8.33%), Space: 56.1 MB (12.5%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.ts
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.ts
@@ -1,15 +1,12 @@
 function removeOccurrences(s: string, part: string): string {
     let stack: string[] = [];
+    let partLen = part.length;
 
     for (let i = 0; i < s.length; i++) {
         stack.push(s[i]);
 
-        if (stack.join('').substring(stack.length - part.length) === part) {
-            let n = part.length;
-            while (n > 0) {
-                stack.pop();
-                n--;
-            }
+        if (stack.length >= partLen && stack.slice(-partLen).join('') === part) {
+            stack.length -= partLen;
         }
     }
 
