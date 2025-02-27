75ae786e1216c65c8cb123dfb4ab12ff21357574
Joe Wehbe <joewehbe@yahoo.com>
2025-02-27 17:45:36 +0200

    Time: 1 ms (71.81%), Space: 58.1 MB (12.08%) - LeetHub

--- 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.ts
+++ 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.ts
@@ -1,19 +1,12 @@
 function countPartitions(nums: number[]): number {
-    let sum = 0;
-    let left = 0;
-    let right = 0;
+    let left = 0, right = nums.reduce((sum, num) => sum += num, 0)
     let result = 0;
-
-    for (const num of nums) {
-        sum += num;
-        right += num;
-    }
 
     for (let i = 0; i < nums.length - 1; i++) {
         left += nums[i];
         right -= nums[i];
 
-        if (Math.abs(left - right) % 2 == 0) result++;
+        if ((left - right) % 2 == 0) result++;
     }
 
     return result;
