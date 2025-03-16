1cbb9490baa40eca2ab82139bfa61c125a9f6a54
Joe Wehbe <joewehbe@yahoo.com>
2025-03-16 15:49:57 +0200

    Time: 1 ms (40%), Space: 55.9 MB (50%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.ts
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.ts
@@ -1,10 +1,12 @@
 function countHillValley(nums: number[]): number {
-    let array = nums.filter((value, index) => index === 0 || value !== nums[index - 1]);
     let count = 0;
+    let prev = nums[0];
 
-    for (let i = 1; i < array.length - 1; i++) {
-        if (array[i] > array[i-1] && array[i] > array[i+1]) count++;
-        else if (array[i] < array[i-1] && array[i] < array[i+1]) count++;
+    for (let i = 1; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i+1]) continue;
+        else if (nums[i] > prev && nums[i] > nums[i+1]) count++;
+        else if (nums[i] < prev && nums[i] < nums[i+1]) count++;
+        prev = nums[i];
     }
 
     return count;
