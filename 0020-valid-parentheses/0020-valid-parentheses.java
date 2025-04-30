0ba1cfa35aa7e0f3e007606f718f8d396d4a5ac3
Joe Wehbe <joewehbe@yahoo.com>
2025-04-30 16:50:05 +0300

    Time: 3 ms (41.82%), Space: 42.4 MB (5.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                    if (!stack.isEmpty()) {
+                    Character peek = stack.peek();
+                    if ((c == ')' && peek != '(') || (c == ']' && peek != '[') || (c == '}' && peek != '{')) return false;
+                    else stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
