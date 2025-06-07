0ae71084bf8b0ec293ffa1dd4f7553b5400c7994
Joe Wehbe <joewehbe@yahoo.com>
2025-06-07 17:36:29 +0300

    Time: 5 ms (53.85%), Space: 44.8 MB (51.88%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,36 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("I", 1);
-        map.put("IV", 4);
-        map.put("V", 5);
-        map.put("IX", 9);
-        map.put("X", 10);
-        map.put("XL", 40);
-        map.put("L", 50);
-        map.put("XC", 90);
-        map.put("C", 100);
-        map.put("CD", 400);
-        map.put("D", 500);
-        map.put("CM", 900);
-        map.put("M", 1000);
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
         int result = 0;
         int i = 0;
 
         while (i < s.length()) {
-            if (i+1 < s.length()) {
-                String sub = s.substring(i, i+2);
-                
-                if (map.containsKey(sub)) {
-                    result += map.get(sub);
-                    i += 2;
-                    continue;
-                }
-            }
+            int curr = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i+1)) : 0; 
 
-            result += map.get(s.substring(i, i+1));
-            i++;
+            if (curr < next) {
+                result += next - curr;
+                i += 2;
+            }
+            else {
+                result += curr;
+                i++;
+            }
         }
 
         return result;
