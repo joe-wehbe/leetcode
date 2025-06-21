4a6f47e9b58e9926f12582251d108a6e785a8cf9
Joe Wehbe <joewehbe@yahoo.com>
2025-06-21 15:42:33 +0300

    Time: 7 ms (80.23%), Space: 45.6 MB (27%) - LeetHub

--- 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.java
+++ 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.java
@@ -1,30 +1,18 @@
 class Solution {
     public boolean checkIfCanBreak(String s1, String s2) {
         int n = s1.length();
-        int[] nums1 = new int[n];
-        int[] nums2 = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            nums1[i] = (int) s1.charAt(i);
-            nums2[i] = (int) s2.charAt(i);
-        }
+        char[] nums1 = s1.toCharArray();
+        char[] nums2 = s2.toCharArray();
 
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
-        return firstBreaks(nums1, nums2) || secondBreaks(nums1, nums2);
+        return canBreaks(nums1, nums2) || canBreaks(nums2, nums1);
     }
 
-    private boolean firstBreaks(int[] nums1, int[] nums2) {
+    private boolean canBreaks(char[] nums1, char[] nums2) {
         for (int i = 0; i < nums1.length; i++) 
             if (nums1[i] < nums2[i]) return false;
-
-        return true;
-    }
-
-    private boolean secondBreaks(int[] nums1, int[] nums2) {
-        for (int i = 0; i < nums1.length; i++)
-            if (nums2[i] < nums1[i]) return false;
 
         return true;
     }
