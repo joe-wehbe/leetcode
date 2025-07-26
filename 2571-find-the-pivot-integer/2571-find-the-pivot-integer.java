1f23da817cd7cdc09443c37a66d48eee06934ddd
Joe Wehbe <joewehbe@yahoo.com>
2025-07-26 23:55:24 +0300

    Time: 1 ms (69.36%), Space: 41.1 MB (27.55%) - LeetHub

---
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int total = n * (n + 1) / 2;
+
+        for (int x = 1; x <= n; x++) {
+            int leftSum = x * (x + 1) / 2;
+            int rightSum = total - (x - 1) * x / 2;
+
+            if (leftSum == rightSum) {
+                return x;
+            }
+        }
+
+        return -1;
+    }
+}
